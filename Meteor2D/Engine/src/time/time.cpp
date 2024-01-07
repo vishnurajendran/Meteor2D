8aca71a7473b19d018dd6c5a00b962ff73c5a344
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2024-01-07 21:15:26 +0530

    added time scale

--- Meteor2D/Engine/src/time/time.cpp
+++ Meteor2D/Engine/src/time/time.cpp
@@ -5,11 +5,12 @@ namespace meteor {
 	uint64_t Time::currFrameTime = 0;
 	uint64_t Time::timeLast = 0;
 	double Time::deltaTime = 0;
+	float Time::timeScale = 1;
 
 	void Time::updateTime() {
 		currFrameTime = SDL_GetTicks();
 		timeLast = timeNow;
 		timeNow = SDL_GetPerformanceCounter();
-		deltaTime = ((double)((timeNow - timeLast) * 1000 / (double)SDL_GetPerformanceFrequency())) * 0.001f;
+		deltaTime = ((double)((timeNow - timeLast) * 1000 / (double)SDL_GetPerformanceFrequency())) * 0.001f * timeScale;
 	}
 }
